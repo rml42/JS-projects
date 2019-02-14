5e0eeaf3f751c19790eebbf35e1a84a317cfd18b
rml42 <serpenthstooth@gmail.com>
2019-02-14 11:51:51 +0200

    Changed CSS look and JS to give class to new elements.

--- OMDB_API/script.js
+++ OMDB_API/script.js
@@ -10,8 +10,17 @@ return parent.appendChild(el);
 const ul = document.getElementById('searchForm');
 
 function findMovie() {
-document.getElementById("searchForm").innerHTML = ""; //clear div when new search is done
-let inputValue = document.getElementById("search").value; //search word
+
+//clear div when new search is done
+    
+document.getElementById("searchForm").innerHTML = "";
+
+//search word from input field
+
+let inputValue = document.getElementById("search").value; 
+
+// fetch magic
+
 const uri = `http://www.omdbapi.com/?s=${inputValue}&apikey=ac918fb5&r`;    
 fetch(uri)
 .then(resp => resp.json())
@@ -19,18 +28,29 @@ fetch(uri)
     movieData = data.Search;
     console.log(movieData);
     return movieData.map(function(movie) {
+    
+    // create new elements
+
     let li = createNode('li'),
     img = createNode('img'),
     h1 = createNode('h1'),
-    span = createNode ('span');
+    p = createNode ('p');
+
+    // append created elements
 
     img.src = movie.Poster;
     h1.innerHTML = `${movie.Title}`;
-    span.innerHTML = `${movie.Type}  </br> </br> ${movie.Year}  </br> </br> <a href="https://www.imdb.com/title/${movie.imdbID}/">ImdbID</a>  </br> </br>   </br> </br> `;
+    p.innerHTML = `Type: ${movie.Type}  </br> </br>Year: ${movie.Year}  </br> </br>Link to Imdb: <a href="https://www.imdb.com/title/${movie.imdbID}/">ImdbID</a>  </br> </br>   </br> </br> `;
     append(li, img);
     append(li, h1);
-    append(li, span);
+    append(li, p);
     append(ul, li);;
+    
+    // give class to new elements
+    
+    h1.className = "listHeader"; 
+    img.className = "listImg"; 
+    p.className = "listSpan"; 
     })
     
 })
