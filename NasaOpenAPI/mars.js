626450ee1add0d8c3b5dca95bb48f0be6d8d4ec4
rml42 <serpenthstooth@gmail.com>
2019-02-14 18:35:04 +0200

    Deleted sounds, update CSS and organiza JS

--- NasaOpenAPI/mars.js
+++ NasaOpenAPI/mars.js
@@ -1,78 +1,81 @@
+// both functions need createNode and append so they are defined first
 
+function createNode(element) {
+    return document.createElement(element);
+}
 
-        function createNode(element) {
-            return document.createElement(element);
-        }
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
-        function append(parent, el) {
-          return parent.appendChild(el);
-        }
-      
-        const ul = document.getElementById('responseContent');
-        const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
-        fetch(url)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let spacePhotos = data.photos;
-            return spacePhotos.map(function(space) {
-              let li = createNode('li'),
-                  img = createNode('img'),
-                  span = createNode('span');
-              img.src = space.img_src;
-              span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
-              append(li, img);
-              append(li, span);
-              append(ul, li);
-          })
-        })
-        .catch(function(error) {
-          console.log(error);
-        }); 
-         
+// get where new content will be put
 
+const ul = document.getElementById('responseContent');
 
-        const ul1 = document.getElementById('responseContent1');
-        const url1 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=NAVCAM&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
-        fetch(url1)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let rovercam = data.photos;
-            return rovercam.map(function(space) {
-              let li = createNode('li'),
-                  img = createNode('img'),
-                  span = createNode('span');
-              img.src = space.img_src;
-              span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
-              append(li, img);
-              append(li, span);
-              append(ul1, li);
-          })
-        })
-        .catch(function(error) {
-          console.log(error);
-        }); 
+// url to fetch
+const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
 
-        const ul2 = document.getElementById('responseContent1');
-        const url2 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=FHAZ&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
-        fetch(url2)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let rovercam = data.photos;
-            return rovercam.map(function(space) {
-              let li = createNode('li'),
-                  img = createNode('img'),
-                  span = createNode('span');
-              img.src = space.img_src;
-              span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
-              append(li, img);
-              append(li, span);
-              append(ul2, li);
-          })
-        })
-        .catch(function(error) {
-          console.log(error);
-        }); 
+// fetch, create and append
 
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    let spacePhotos = data.photos;
+    return spacePhotos.map(function(space) {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = space.img_src;
+      span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+}); 
+  
+// second image row
 
-        var menuClick = new Audio();
-        menuClick.src = "sounds/CLICK10A.WAV"
+const ul1 = document.getElementById('responseContent1');
+const url1 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=NAVCAM&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
+fetch(url1)
+.then((resp) => resp.json())
+.then(function(data) {
+    let rovercam = data.photos;
+    return rovercam.map(function(space) {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = space.img_src;
+      span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
+      append(li, img);
+      append(li, span);
+      append(ul1, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+}); 
+
+const ul2 = document.getElementById('responseContent1');
+const url2 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=FHAZ&api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
+fetch(url2)
+.then((resp) => resp.json())
+.then(function(data) {
+    let rovercam = data.photos;
+    return rovercam.map(function(space) {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = space.img_src;
+      span.innerHTML = `${space.camera.full_name} ${space.earth_date}`;
+      append(li, img);
+      append(li, span);
+      append(ul2, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+}); 
--- NasaOpenAPI/scripts.js
+++ NasaOpenAPI/scripts.js
@@ -1,38 +1,54 @@
 
+  // url to fetch
 
-        function createNode(element) {
-            return document.createElement(element);
-        }
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
+  
+  // fetch
+  
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+  
+  // get where the new content will be placed in document  
 
-        function append(parent, el) {
-          return parent.appendChild(el);
-        }
-        const ul = document.getElementById('responseContent');
-        const url = 'https://api.nasa.gov/planetary/apod?api_key=S9wIHzSzlzZsbGLjkkVYIJYfvXLpQKiJyZ0Jdnar';
-        fetch(url)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let apod = data
-            let li = createNode('li'),
-            img = createNode('img'),
-            h1 = createNode('h1')
-            span = createNode('span');
-        
-        img.src = apod.url;
-        h1.innerHTML = `${apod.title}`;
-        span.innerHTML = `${apod.explanation} </br> </br> ${apod.copyright}`;
-        append(li, img);
-        append(li, h1);
-        append(li, span);
-        append(ul, li);
-        })
-        .catch(function(error) {
-          console.log(error);
-        }); 
+  const ul = document.getElementById('responseContent');
+  
+  // create new element function
+
+  function createNode(element) {
+      return document.createElement(element);
+    }
+
+  // append new elements to parents
+
+  function append(parent, el) {
+    return parent.appendChild(el);
+    }
+
+  // use create function
+
+  let apod = data
+  let li = createNode('li'),
+  img = createNode('img'),
+  h1 = createNode('h1')
+  span = createNode('span');
+  
+  // content and append
+
+  img.src = apod.url;
+  h1.innerHTML = `${apod.title}`;
+  span.innerHTML = `${apod.explanation} </br> </br> ${apod.copyright}`;
+  append(li, img);
+  append(li, h1);
+  append(li, span);
+  append(ul, li);
+  })
+  .catch(function(error) {
+    console.log(error);
+  }); 
 
 
-    var menuClick = new Audio();
-    menuClick.src = "sounds/CLICK10A.WAV"
+
 
     
     
